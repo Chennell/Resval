e841fabd8709f82fcfb2ef34870fb943a2aa8397
BKBjorns <32642533+BKBjorns@users.noreply.github.com>
2017-11-27 13:54:14 +0100

    Logo in header

--- partials/sitehead.php
+++ partials/sitehead.php
@@ -1,6 +1,14 @@
+<?php $homeurl = home_url(); ?>
+
 <header>
+  <div id="logocontainer">
+    <a href="<?php echo $homeurl; ?>">
+      <img src="<?php header_image();?>" alt="" class="logo"/>
+    </a>
+  </div>
+
     <!-- <a href="<?php home_url(); ?>"><h1><?php /* gets the site name*/bloginfo('name'); ?></h1></a> -->
-  
+
     <div id="mainmenu">
       <nav>
            <?php wp_nav_menu( array( 'theme_location' => 'header-nav' ) );?>

d8652f7093988ccfd2d252d90f2dcfbef03dc44e
BKBjorns <32642533+BKBjorns@users.noreply.github.com>
2017-11-29 19:37:55 +0100

    Front page
    Missing the Most popular view pictures

--- front-page.php
+++ front-page.php
@@ -3,39 +3,62 @@
 
 <div class="frontPageContainer">
 
-  <!--    Top section   ------------------------------------------------->
-  <!--    The image      -->
-  <div class="div_topp_img_hem">
-    <img src="<?php the_field('topp_bild'); ?>" alt="" class="topp_img_hem">
+  <!--    Top section About section  ------------------------------------------------->
+  <div class="topSectionFP">
+    <div class="topLeftFP">
+      <!--    The image      -->
+      <div class="div_topp_img_hem">
+        <img src="<?php the_field('topp_bild'); ?>" alt="" class="topp_img_hem">
+      </div>
+    </div>
+
+    <div class="topRightFP">
+      <!-- The text -->
+      <div class="div_topp_text_hem">
+        <div class="title_FP">
+          <h2> <?php the_field('topp_titel'); ?></h2>
+        </div>
+
+        <p><?php the_field('topp_text'); ?></p>
+
+        <!-- The button -->
+        <div class="buttonContainerFP">
+          <button type="button" name="button">
+            <a href="<?php the_field('fremsid_knapp'); ?>"><?php the_field('kontakt_knapp_label'); ?></a>
+          </button>
+        </div>
+      </div>
+    </div>
+
   </div>
 
-  <!-- The text -->
-  <div class="div_topp_text_hem">
-    <h2> <?php the_field('topp_titel'); ?></h2>
-
-    <p><?php the_field('topp_text'); ?></p>
-  </div>
-
-  <!-- The button -->
-
-  <button type="button" name="button">
-    <a href="">Text here</a>
-  </button>
 
 
   <!--  Middle section     -------------------------------->
   <!--  Most popular views   -->
   <div class="mostPopularProjectsFP">
-    <h2>Most popular projects</h2>
+    <div class="title_FP">
+      <h2>Most popular projects</h2>
+    </div>
+    <ul>
+      <li>
+        <div class="">
+          <p>Image<br> here!</p>
+      </div>
+      </li>
+    </ul>
   </div>
 
 
 
   <!--  Last section     -------------------------------->
   <!--  Contact info   -->
-<div class="contactInfoFP">
-  <h2> <?php the_field(''); ?></h2>
-</div>
+  <div class="contactInfoFP">
+    <div class="title_FP">
+      <h2><?php the_field('kontakt_titel'); ?></h2>
+    </div>
+    <p><?php the_field('kontakt_text'); ?></p>
+  </div>
 
 
 </div>

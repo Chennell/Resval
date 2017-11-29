95e85923be55f9d8fc020cd3995672275bfa041d
Chennell <jessica_chennell@hotmail.com>
2017-11-29 21:27:54 +0100

    Fixing search and filters

--- archive.php
+++ archive.php
@@ -2,39 +2,53 @@
 
     get_header()?>
     <div class="post-grid">
+        <div class="search_form">
+            <?php
+                get_search_form();
+            ?>
+        </div>
+
+        <?php
+        $args = array(
+          'post_type'       =>  'project',
+          'posts_per_page'  => -1,
+        );
+        $project_query = new WP_Query($args);
+
+        //$my_query = new WP_Query($args);?>
+        <div class="filterbox">
+            <ul class="project_type"><?php
+        //--- GETS the post types, filters.
+        $terms = get_terms(array('taxonomy' => 'project', 'hide_empty' => true ));
+        foreach ($terms as $value) { 
+            ?>
+           
+                    <li class="filter">
+                        <a href="/project_type/<?php echo $value->slug ?>">
+                            <?php echo $value->name ?>
+                        </a>
+                    </li>
+            
     <?php
-    $args = array(
-      'post_type'       =>  'project',
-      'posts_per_page'  => -1,
-    );
-    $project_query = new WP_Query($args);
+        }
 
-    //$my_query = new WP_Query($args);
-      
-    //--- GETS the post types, filters.
-    $terms = get_terms(array('taxonomy' => 'project', 'hide_empty' => true ));
-    foreach ($terms as $value) { 
-    ?>
-    <span class="project_type">
-        <a href="/project_type/<?php echo $value->slug ?>">
-            <?php echo $value->name ?>
-        </a>
-    </span>
-<?php
-    }
-
-?>
+?>    
+       </ul>
+    </div>
 <!--THIS loops through and displays the projects-->
-<div class="gridParent">
-    <?php
-    
-    if ($project_query->have_posts()): while ($project_query->have_posts()): $project_query->the_post();
-
-        get_template_part('partials/project-grid');
-
-        endwhile; endif;
-    ?>
-</div>
+    <ul class="gridParent">
+        <?php
+            if ($project_query->have_posts()): while ($project_query->have_posts()): $project_query->the_post();
+        ?>
+            <li>
+               <?php
+                    get_template_part('partials/project-grid');
+                ?>
+            </li>
+        <?php
+            endwhile; endif;
+        ?>
+    </ul>
 </div> 
 
 <?php

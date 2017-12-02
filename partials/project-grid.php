ddf904fad4f6ffc7836449c90f13fd469322554c
Chennell <jessica_chennell@hotmail.com>
2017-12-02 16:26:06 +0100

    Archive hover kind of failing

--- partials/project-grid.php
+++ partials/project-grid.php
@@ -2,10 +2,13 @@
     <a href="<?php the_permalink()?>">
         <h3 class="title-grid"> <?php the_title(); ?></h3>
     </a>
-    <div class="info-box">
-        <p>info some lines</p>
-    </div>
     <span class="project-grid-pic">
+        <div class="info-box">
+            <p>
+                This is some random content about the project.
+            </p>
+        </div>
+
         <a href="<?php the_permalink()?>"><?php the_post_thumbnail('grid_thumbnail'); ?></a>
     </span>
 </span> 

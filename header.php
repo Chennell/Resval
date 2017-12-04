8754d24a8cf844e98a783d6109eededf296bcbeb
KarolinLuneburg <32455233+karolin@users.noreply.github.com>
2017-12-04 13:54:35 +0100

    footer fix

--- header.php
+++ header.php
@@ -4,9 +4,6 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0" />
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-    <script src="<?= get_template_directory_uri(); ?>/js/jquery.flexslider.js" type="text/javascript"></script>
-    <script src="<?= get_template_directory_uri(); ?>/js/jquery-1.7.1.min.js" type="text/javascript"></script>
 
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>" type="text/css">
     <?php wp_head() ?>

0fcfb595d22a5d27784485ec5861c45e4b73123e
Chennell <jessica_chennell@hotmail.com>
2017-12-09 19:31:54 +0100

    search

--- partials/project-grid.php
+++ partials/project-grid.php
@@ -2,13 +2,15 @@
 <a href="<?php the_permalink()?>">
     <div class="opacityBox"><h3 class="title-grid"> <?php the_title(); ?></h3></div>
 </a>
-<div class="info-box">
-    <p>
-       <?php
-            the_field(hover_description);
-        ?>
-    </p>
-</div>
+<a href="<?php the_permalink()?>">
+    <div class="info-box">
+        <p>
+           <?php
+                the_field(hover_description);
+            ?>
+        </p>
+    </div>
+</a>
 <a href="<?php the_permalink()?>">
     <?php the_post_thumbnail('grid_thumbnail'); ?>
 </a>
---
+++ search.php
@@ -0,0 +1,63 @@
+<?php
+
+    get_header()?>
+    <main>
+    <div class="post-grid">
+        <div class="search_form">
+            <?php
+                get_search_form();
+            ?>
+        </div>
+
+        <?php
+        $args = array(
+          'post_type'       =>  'project',
+          'posts_per_page'  => -1,
+        );
+        $project_query = new WP_Query($args);
+
+        //$my_query = new WP_Query($args);?>
+        <div class="filterbox">
+            <div class="project_type"><?php
+        //--- GETS the post types, categories.
+        $terms = get_terms(array('taxonomy' => 'project-cat', 'hide_empty' => true ));
+        $terms = get_terms(array('taxonomy' => 'project-topic', 'hide_empty' => true ));
+        $terms = get_terms(array('taxonomy' => 'project-cost', 'hide_empty' => true ));
+        $terms = get_terms(array('taxonomy' => 'project-difficulty', 'hide_empty' => true ));
+        $terms = get_terms(array('taxonomy' => 'project-time', 'hide_empty' => true )); ?>
+
+          <div class="filters">
+          <?php echo do_shortcode( '[searchandfilter class="filters-form" type="checkbox,checkbox,checkbox,checkbox" hide_empty="0, 0, 0, 0" hierarchical="0, 0, 0, 0" taxonomies="project-topic,project-cost,project-time,project-difficulty"  headings="Topic,Cost,Time,Difficulty"]' ); ?>
+        </div>
+
+      </div>
+    </div>
+<!--THIS loops through and displays the projects-->
+    <ul class="gridParent">
+        <?php
+            if (have_posts()): while (have_posts()): the_post();
+        ?>
+            <li class="a-project">
+               <?php
+                    get_template_part('partials/project-grid');
+                ?>
+            </li>
+        <?php
+            endwhile; 
+                else: echo "<h2 class='resultat-error'> Din sökning gav inga resultat</h2>";
+            endif;
+        ?>
+    </ul>
+</div>
+</main>
+<?php
+    get_footer()
+?>
+
+
+
+
+
+<!--
+"<a href="/project-cat/<?php echo $value->slug ?>">
+</a>"-->

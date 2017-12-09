6f9be2eeffcad685f3a482d0015d5d7d90f1e03c
KarolinLuneburg <32455233+karolin@users.noreply.github.com>
2017-12-09 11:29:20 +0100

    scroll icon

--- front-page.php
+++ front-page.php
@@ -1,144 +1,146 @@
 <!--      Header        -->
 <?php get_header();?>
+<main>
+  <div class="frontPageContainer">
+    <?php do_shortcode(
+      '[wcp-carousel id="57"]'
+      ) ?>
 
-<div class="frontPageContainer">
-  <?php do_shortcode(
-    '[wcp-carousel id="57"]'
-    ) ?>
 
-
-  <!--    Top section About section  ------------------------------------------------->
-  <div class="topSectionFP">
-    <div class="topLeftFP">
-      <!--    The image      -->
-      <div class="div_topp_img_hem">
-        <img src="<?php the_field('topp_bild'); ?>" alt="" class="topp_img_hem">
-      </div>
-    </div>
-
-    <div class="topRightFP">
-      <!-- The text -->
-      <div class="div_topp_text_hem">
-        <div class="title_FP">
-          <h2> <?php the_field('topp_titel'); ?></h2>
-        </div>
-
-        <p><?php the_field('topp_text'); ?></p>
-
-        <!-- The button -->
-        <div class="buttonContainerFP">
-          <button type="button" name="button">
-            <a href="<?php the_field('fremsid_knapp'); ?>"><?php the_field('fremsida_knapp_label'); ?></a>
-          </button>
+    <!--    Top section About section  ------------------------------------------------->
+    <div class="topSectionFP">
+      <div class="topLeftFP">
+        <!--    The image      -->
+        <div class="div_topp_img_hem">
+          <img src="<?php the_field('topp_bild'); ?>" alt="" class="topp_img_hem">
         </div>
       </div>
+
+      <div class="topRightFP">
+        <!-- The text -->
+        <div class="div_topp_text_hem">
+          <div class="title_FP">
+            <h2> <?php the_field('topp_titel'); ?></h2>
+          </div>
+
+          <p><?php the_field('topp_text'); ?></p>
+
+          <!-- The button -->
+          <div class="buttonContainerFP">
+            <button type="button" name="button">
+              <a href="<?php the_field('fremsid_knapp'); ?>"><?php the_field('fremsida_knapp_label'); ?></a>
+            </button>
+          </div>
+        </div>
+      </div>
+
     </div>
 
-  </div>
 
 
+    <!--  Middle section     -------------------------------->
+    <!--  Most popular views   -->
+    <div class="mostPopularProjectsFP">
+      <!-- <div class="title_FP">
+        <h2>Most popular projects</h2>
+      </div>
 
-  <!--  Middle section     -------------------------------->
-  <!--  Most popular views   -->
-  <div class="mostPopularProjectsFP">
-    <!-- <div class="title_FP">
-      <h2>Most popular projects</h2>
+
+  <!-- PROJECT SLIDER ----------------------------------------------------------->
+      <!-- Carousel slider -->
+      <?php //the_content(); ?>
     </div>
 
+    <!-- Flexslider -->
 
-<!-- PROJECT SLIDER ----------------------------------------------------------->
-    <!-- Carousel slider -->
-    <?php //the_content(); ?>
-  </div>
+    <div class="project-slider">
+      <div class="flexslider">
+        <ul class="slides">
+      <?php
+         if( have_rows('project_slider') ):
+        // loop through the rows of data
+        while ( have_rows('project_slider') ) : the_row();
+          $project = get_sub_field('project_on_front_page');
+          $projectTitle = $project->post_title;
+          $projectImage = get_the_post_thumbnail($project, 'about_logo');
+          $projecturl = get_permalink($project)
+        ?>
+        <!-- Place somewhere in the <body> of your page -->
+           <li class="singleProjectSlider">
+               <div class="projectSliderdiv">
 
-  <!-- Flexslider -->
+               <span class="">
+                  <?php echo $projectImage; ?>
+                 <a href="<?php echo $projecturl ?>">
+                   <h2>
+                     <?php echo $projectTitle; ?>
+                   </h2>
+                   </a>
+              </span>
+               </div>
+           </li>
+           <?php endwhile; endif;?>
+         </ul>
+       </div>
+    </div>
 
-  <div class="project-slider">
+    <!-- Sponsor Area -->
+    <!-- <div class="sponsor-area">
+      <?php
+         //if( have_rows('sponsrade_loggor_repeater') ):
+        // loop through the rows of data
+        //while ( have_rows('sponsrade_loggor_repeater') ) : the_row(); ?>
+        <!-- Place somewhere in the <body> of your page -->
+       <!-- <div class="flexslider">
+         <ul class="slides">
+           <li class="sponsor-container">
+             <a href="<?php //the_sub_field('sponsrade_url'); ?>">
+               <img class="sponsor-logo" src="<?php //echo wp_get_attachment_image_url(the_sub_field('sponsrade_loggor'), 'about_logo'); ?>" />
+             </a>
+           </li>
+         </ul>
+       </div> -->
+       <?php //endwhile; endif;?>
+  <!--  </div> -->
+
+  <div class="image-slider">
     <div class="flexslider">
       <ul class="slides">
-    <?php
-       if( have_rows('project_slider') ):
-      // loop through the rows of data
-      while ( have_rows('project_slider') ) : the_row();
-        $project = get_sub_field('project_on_front_page');
-        $projectTitle = $project->post_title;
-        $projectImage = get_the_post_thumbnail($project, 'about_logo');
-        $projecturl = get_permalink($project)
-      ?>
-      <!-- Place somewhere in the <body> of your page -->
-         <li class="singleProjectSlider">
-             <div class="projectSliderdiv">
-
-             <span class="">
-                <?php echo $projectImage; ?>
-               <a href="<?php echo $projecturl ?>">
-                 <h2>
-                   <?php echo $projectTitle; ?>
-                 </h2>
-                 </a>
-            </span>
-             </div>
-         </li>
-         <?php endwhile; endif;?>
+        <?php if (have_posts()): while (have_posts()): the_post();?>
+        <!-- include featured image -->
+        <li class="project-image">
+            <?php the_post_thumbnail( 'grid_thumbnail' ); ?>
+            <span class="project-image-caption"><?php the_sub_field('image_description') ?></span>
+        </li>
+        <?php endwhile; endif;?>
        </ul>
      </div>
   </div>
 
-  <!-- Sponsor Area -->
-  <!-- <div class="sponsor-area">
-    <?php
-       //if( have_rows('sponsrade_loggor_repeater') ):
-      // loop through the rows of data
-      //while ( have_rows('sponsrade_loggor_repeater') ) : the_row(); ?>
-      <!-- Place somewhere in the <body> of your page -->
-     <!-- <div class="flexslider">
-       <ul class="slides">
-         <li class="sponsor-container">
-           <a href="<?php //the_sub_field('sponsrade_url'); ?>">
-             <img class="sponsor-logo" src="<?php //echo wp_get_attachment_image_url(the_sub_field('sponsrade_loggor'), 'about_logo'); ?>" />
-           </a>
-         </li>
-       </ul>
-     </div> -->
-     <?php //endwhile; endif;?>
-<!--  </div> -->
 
-<div class="image-slider">
-  <div class="flexslider">
-    <ul class="slides">
-      <?php if (have_posts()): while (have_posts()): the_post();?>
-      <!-- include featured image -->
-      <li class="project-image">
-          <?php the_post_thumbnail( 'grid_thumbnail' ); ?>
-          <span class="project-image-caption"><?php the_sub_field('image_description') ?></span>
-      </li>
-      <?php endwhile; endif;?>
-     </ul>
+    <!--  Last section     -------------------------------->
+    <!--  Contact info   -->
+   <div class="contactbackground">
+
+
+    <div class="contactInfoFP">
+      <div class="title_FP">
+        <h2><?php the_field('kontakt_titel'); ?></h2>
+      </div>
+      <p><?php the_field('kontakt_text'); ?></p>
+
+      <!-- The button -->
+      <div class="contactButtonContainerFP">
+        <button type="button" name="button">
+          <a href="<?php the_field('kontakt_knapp'); ?>"><?php the_field('kontakt_knapp_label'); ?></a>
+        </button>
+      </div>
+    </div>
    </div>
-</div>
 
 
-  <!--  Last section     -------------------------------->
-  <!--  Contact info   -->
- <div class="contactbackground">
-
-
-  <div class="contactInfoFP">
-    <div class="title_FP">
-      <h2><?php the_field('kontakt_titel'); ?></h2>
-    </div>
-    <p><?php the_field('kontakt_text'); ?></p>
-
-    <!-- The button -->
-    <div class="contactButtonContainerFP">
-      <button type="button" name="button">
-        <a href="<?php the_field('kontakt_knapp'); ?>"><?php the_field('kontakt_knapp_label'); ?></a>
-      </button>
-    </div>
   </div>
- </div>
 
-
-</div>
+</main>
 <!--     Footer       -->
 <?php get_footer(); ?>
--- js/script.js
+++ js/script.js
@@ -7,14 +7,14 @@ var framgangsfaktor = document.getElementsByClassName("framgangsfaktor");
 var framgangsfaktorTrigger= document.getElementsByClassName('framgangsfaktor-trigger');
 
 for (var i = 0; i<expandTrigger.length; i++){
-    expandTrigger[i].addEventListener("click" ,function (event){
+    expandTrigger[i].addEventListener("click" ,function expander (event){
     this.parentNode.childNodes[3].classList.toggle('show');
     this.firstElementChild.classList.toggle('expander-modifier');
 
 });}
 
 for (var i = 0; i<framgangsfaktorTrigger.length; i++){
-    framgangsfaktorTrigger[i].addEventListener("click" ,function (event){
+    framgangsfaktorTrigger[i].addEventListener("click" ,function framgangsfaktor (event){
     this.parentNode.childNodes[3].classList.toggle('show');
     this.classList.toggle('expander-modifier');
 
@@ -35,8 +35,8 @@ jQuery(window).load(function() {
 
 
 
-// -- CATEGORY ICONS
-var filters = document.getElementsByClassName('filters-form')
+-- CATEGORY ICONS
+var filters = document.getElementsByClassName('filters-form');
 var iconArea = document.getElementsByClassName('icon-area');
 var topic_bike = document.getElementsByClassName('cat-item-14');
 
@@ -49,3 +49,17 @@ for (var i = 0; i<categoryTrigger.length; i++){
     categoryTrigger[i].addEventListener("click" ,function (event){
     event.currentTarget.nextElementSibling.classList.toggle('show');
 });}
+
+
+// // -- Scroll icon anmiation only when overflow scroll available
+// var scrollarea = document.getElementsByClassName('scroll-y');
+// var scrollicon = document.getElementsByClassName('fa fa-arrow-circle-down');
+//
+//
+//
+// if (jQuery("scrollarea").prop('scrollWidth') > jQuery("scrollarea").width() ) {
+//   this.classList.toggle('show');
+// }
+// else {
+//  alert("this element is not overflowing!!");
+// }

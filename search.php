94818b3bb744336fc8fa6371b50651fdec6af6bc
Chennell <jessica_chennell@hotmail.com>
2017-12-10 19:38:00 +0100

    Icons again

--- search.php
+++ search.php
@@ -20,14 +20,16 @@
         <div class="filterbox">
             <div class="project_type"><?php
         //--- GETS the post types, categories.
-        $terms = get_terms(array('taxonomy' => 'project-cat', 'hide_empty' => true ));
+       $terms = get_terms(array('taxonomy' => 'project-cat', 'hide_empty' => true ));
         $terms = get_terms(array('taxonomy' => 'project-topic', 'hide_empty' => true ));
         $terms = get_terms(array('taxonomy' => 'project-cost', 'hide_empty' => true ));
         $terms = get_terms(array('taxonomy' => 'project-difficulty', 'hide_empty' => true ));
-        $terms = get_terms(array('taxonomy' => 'project-time', 'hide_empty' => true )); ?>
+        $terms = get_terms(array('taxonomy' => 'project-time', 'hide_empty' => true )); 
+        $terms = get_terms(array('taxonomy' => 'project-location', 'hide_empty' => true ));
+        $terms = get_terms(array('taxonomy' => 'project-organisation', 'hide_empty' => true )); ?>
 
           <div class="filters">
-          <?php echo do_shortcode( '[searchandfilter class="filters-form" type="checkbox,checkbox,checkbox,checkbox,checkbox,checkbox" hide_empty="0, 0, 0, 0, 0, 0" hierarchical="0, 0, 0, 0, 0, 0" fields="project-topic,project-cost,project-time,project-difficulty,project-location,project-organisation"  headings="Ämne,Kostnad,Tid,Svårighetsgrad, Mest lämpad plats, Mest lämpad organisationstyp"]' ); ?>
+              <?php echo do_shortcode( '[searchandfilter class="filters-form" type="checkbox,checkbox,checkbox,checkbox,checkbox,checkbox" hide_empty="0, 0, 0, 0, 0, 0" hierarchical="0, 0, 0, 0, 0, 0" fields="project-topic,project-cost,project-time,project-difficulty,project-location,project-organisation"  headings="Ämne,Kostnad,Tid,Svårighetsgrad, Mest lämpad plats, Mest lämpad organisationstyp"]' ); ?>
         </div>
 
       </div>
